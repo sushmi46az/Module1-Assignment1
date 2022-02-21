ad8813eaca963d3ed78161e693e9ea194a4d470b
unknown <sushmi46az@gmail.com>
2022-02-21 18:02:57 +0400

    logic update

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-(function(){
+(function () {
+    'use strict;'
 
-angular.module('FixTypo',[])    
-.controller('FixTypoController',function ($scope)
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope)
+   /* .controller('LunchCheckController',function ($scope)*/
 {
 $scope.Name="Module1"
 $scope.ItemList=function(){
     console.log("entered itemlist")
-var totalString= getStringCount($scope.inputVal);
-var input=document.getElementById('txtInput');
-if(totalString <= 3)
+    console.log("input val" + $scope.inputVal.trim());
+var totalString= getStringCount($scope.inputVal.trim());
+    var input = document.getElementById('txtInput');
+    if (totalString <= 3 && totalString !=0)
 {
     input.style.color="green";
     alert("Enjoy !")
 }
-else{
+    else if (totalString > 3) {
     input.style.color="red";
     alert("Too Much")
 }
@@ -22,26 +27,39 @@ $scope.totalStringval=totalString;
 }
 function getStringCount(string)
 {
-    var arr=[];
-    var totalStringValue="";
-    for (var i = 0; i < string.length; i++) {
-        console.log("entered getStringCount")
-        console.log("string.charAt(i) :"+string.charAt(i))
-        if(string.charAt(i)=="," || i==(string.length - 1))
-        {
-            arr.push(totalStringValue);
+    console.log("string initial length "+string.trim().length);
+    var arr = [];
 
-            totalStringValue ="";
+
+        var totalStringValue = "";
+
+        for (var i = 0; i < string.length; i++) {
+    
+            console.log("string.charAt(i) :" + string.charAt(i))
+            if (string.charAt(i).trim().length > 0) {
+                if (string.charAt(i) == ",") {
+                    if (totalStringValue.length > 0)
+                        arr.push(totalStringValue);
+
+                    totalStringValue = "";
+                    console.log(totalStringValue)
+                }
+                
+                else  {
+                    console.log("else " + string.charAt(i))
+                    totalStringValue += string.charAt(i);
+                    console.log(totalStringValue)
+                }
+                if (i == (string.length - 1)) {
+                    if (totalStringValue.length > 0)
+                        arr.push(totalStringValue);
+                }
+            }
         }
-        else{
-            console.log("else")
-            totalStringValue += string.charAt(i);
-            console.log(totalStringValue)
-        }
-       
-      }
+
+
       return arr.length;
 }
 
-});
+}
 })();
